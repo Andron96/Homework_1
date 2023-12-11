a60804afd2f1336394a0f1e31ee09bfef0b96fc0
Andron96 <drandarik2@gmail.com>
2023-12-11 17:17:36 +0200

    2

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-$a = 1.54;
-$b = "письмо от друга";
-$c = false;
-$d = "";
-var_dump($a);
-echo "<br>";
-var_dump($b);
-echo "<br>";
-var_dump($c);
-echo "<br>";
-var_dump($d);
+$name = readline("Введите Ваше имя и не выебуйстесь: ");
+echo "$name, а значете что? Вы ниче так нормальный пацик/типуха.\n\n";
+echo "Ану ка теперь введите сколько угодно значений для вычисления их среднего арифметического.\n";
+echo "Когда закончите вводить значения напишите \"Вычислить\".\n";
+$cycle = true;
+$elements = [];
+$sum = 0;
+$amount = 0;
+$average = 0;
+while ($cycle) {
+    $inpVal = readline("Введите целое число от 1 до 100 или \"Вычислить\": ");
+    if (($inpVal >= 1) && ($inpVal <= 100)) {
+        $elements[] = $inpVal;
+    }
+    elseif ($inpVal === "Вычислить") {
+        foreach ($elements as $elementsVal) {
+            $sum += $elementsVal;
+        }
+        $amount = count($elements);
+        if ($amount == 0) {
+            break;
+        }
+        $average = $sum / $amount;
+        break;
+    }
+    else {
+        break;
+    }
+}
+echo "Сумма элементов равна: $sum.\n";
+echo "Количество элементов равно: $amount.\n";
+echo "Сренее арифметическое равно: $average.\n";
